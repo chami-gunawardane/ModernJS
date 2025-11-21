23df87675d9088f826c99fce325045e2213af15a
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-22 01:13:08 +0530

    Spread & Rest Operator

---
+++ SpreadAndRestOperator/app.js
@@ -0,0 +1,11 @@
+// The ... operator is two things:
+// Spread → expands an array or object
+// Rest → collects multiple items into one variable
+
+//Spread with arrays
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5];
+
+console.log(arr2); 
+
+//Test 01
