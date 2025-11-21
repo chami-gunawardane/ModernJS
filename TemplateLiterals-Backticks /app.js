fd4cb0514365966bd906889e33425ca4008e5bc8
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-22 00:24:46 +0530

    Part 03 Done

--- TemplateLiterals-Backticks /app.js
+++ TemplateLiterals-Backticks /app.js
@@ -1,3 +1,41 @@
 //Old way
 const name = "Chami";
 console.log("Hello " + name + "!");
+
+//new way
+console.log(`Hello ${name}!`);
+
+//Multiple variables inside
+const first = "Chami";
+const last = "Gunawardane";
+console.log(`Full name: ${first} ${last}`);
+
+//Multi line strings
+const message = `
+This is line one
+This is line two
+This is line three
+`;
+console.log(message);
+
+//Expressions inside strings
+const a = 10;
+const b = 5;
+
+console.log(`Total: ${a + b}`);
+
+//PRACTICE TASKS
+const fristName = "Chami";
+console.log(`Hi ${fristName}, Welcome back!`);
+
+let price = 4500;
+let qty = 3;
+console.log(`You ordered ${qty} Items. Total price is Rs.${price}`);
+
+const ManageEvent = `
+Event:Gold Package
+Type: Premium
+Status: Confirmed
+`;
+console.log(ManageEvent);
+
