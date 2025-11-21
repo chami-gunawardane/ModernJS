a8de713bf38ac8b59adfae48f592bbb38f18af95
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-21 23:28:03 +0530

    Variable Declaration

---
+++ VariableDeclaration/app.js
@@ -0,0 +1,14 @@
+let count = 5;
+count = 100; //allowed
+console.log(count); // 100
+
+const price = 70;
+// price = 80; //error
+console.log(price);
+
+const user = { name : "Chami"};
+user.name = "Anna"
+console.log(user.name);
+
+// user.name
+
