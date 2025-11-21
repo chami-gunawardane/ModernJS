ff96a3e38adadfe5f94500b76323349b7a9bb656
chami-gunawardane <chamigunawardane@gmail.com>
2025-11-21 23:35:14 +0530

    Part 01 done

--- VariableDeclaration/app.js
+++ VariableDeclaration/app.js
@@ -10,5 +10,13 @@ const user = { name : "Chami"};
 user.name = "Anna"
 console.log(user.name);
 
-// user.name
+// user.name // error
+
+let a = 10;
+a = 20;
+console.log(a);
+
+const name = "Chami";
+name = "Ishara";
+
 
